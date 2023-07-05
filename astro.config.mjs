28774b19d06c93ac9c56a95cc89c49bcb5f1dcb0
vivekneupane11 <vivekneupane11@gmail.com>
2023-07-06 02:26:28 +0545

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          entryFileNames: 'entry.[hash].js',
-          chunkFileNames: 'chunks/chunk.[hash].js',
-          assetFileNames: 'assets/asset.[hash][extname]',
-        },
-      },
-    },
-  },
+  base:'dist',
   integrations: [react(), tailwind()]
 });
